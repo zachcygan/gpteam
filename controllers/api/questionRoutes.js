dfac4f3a32f8d7c392511fbef0f9334de2b675ca
Zachary Cygan <zachcygan@gmail.com>
2023-04-06 20:37:18 -0700

    completed question routes

--- controllers/api/questionRoutes.js
+++ controllers/api/questionRoutes.js
@@ -12,6 +12,32 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await Comment.destroy({
+            where: {
+                question_id: req.params.id
+            }
+        });
+
+        const postData = await Question.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        })
+
+        if (!postData) {
+            req.status(400).json({ message: 'No post found with that id' });
+            return;
+        }
+
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 
