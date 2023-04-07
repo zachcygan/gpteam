f39353d64d3d6858a2d81c466ca33fa2cfbd74fe
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-04-06 18:16:08 -0700

    login/signup feauture

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
     res.render('homepage', {
     })
 })
+
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
