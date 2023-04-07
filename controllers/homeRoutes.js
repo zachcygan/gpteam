4010483e627b263460746ba38bf6a676debfdd0f
Zachary Cygan <zachcygan@gmail.com>
2023-04-06 18:38:58 -0700

    commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Comment, Document, Question, User } = require('../models')
 
 router.get('/', async (req, res) => {
     res.render('homepage', {
