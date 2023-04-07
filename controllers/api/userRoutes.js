343032df442892e654e7062ade7d9db751c3743c
Zachary Cygan <zachcygan@gmail.com>
2023-04-06 19:34:58 -0700

    added /register, /comment, /login, and /logout routes to allow users to login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -43,4 +43,42 @@ router.post('/login', async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const userData = await User.create({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        res.redirect('/login');
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end()
+    }
+});
+
+router.post('/comment', async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            content: req.body.content,
+            post_id: req.session.post_id,
+            user_id: req.session.user_id
+        })
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
