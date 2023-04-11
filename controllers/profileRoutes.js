eaa906b86567bb242b262fe261d830c99c8189eb
Zachary Cygan <zachcygan@gmail.com>
2023-04-11 14:12:39 -0700

    added posts on individual user page

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -44,15 +44,23 @@ router.get('/user/:id', withAuth, async (req, res) => {
             ],
         });
 
+        const docData = await Document.findAll({
+            where: {
+                user_id: req.params.id
+            }
+        })
+
         if (!userData) {
             res.status(404).json({ message: 'No user found with this id!' });
             return;
         }
 
+        const documents = docData.map((document) => document.get({ plain: true }));
         const user = userData.get({ plain: true });
 
         res.render('userprofile', {
             user,
+            documents,
             logged_in: req.session.logged_in
         });
     } catch (err) {
